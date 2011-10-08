d97c2ebb2f74dffd4346b435dc8904620b3d8cdf
MIUI Android <markh@miuiandroid.com>
2011-10-08 15:03:51 +0100

    Emergency fix for OTA functions thanks to kurisu for the fix :-)

--- libs/ota_functions.php
+++ libs/ota_functions.php
@@ -4,8 +4,10 @@
 // @param current_version (users currently installed MIUI version e.g. 1.8.12)
 function current_rom($current_version, $hardware_board, $device_name)
 {
+	/*
 	// SQL
-	$sql = "SELECT * FROM devices WHERE version > '$current_version' AND device='$device_name' AND board='$hardware_board' LIMIT 1";
+	$sql = "SELECT * FROM devices WHERE version > '$current_version' AND device='$device_name' AND board='$hardware_board' 
+LIMIT 1";
 
 	// Query
 	$query = mysql_query($sql)or die(mysql_error());
@@ -17,16 +19,43 @@ function current_rom($current_version, $hardware_board, $device_name)
 	$get_latest_version = mysql_fetch_assoc($query);
 
 	if($num_rows > 0) return true;
+	*/
+	
+	
+	// select all versions for device
+	$sql = "SELECT version FROM devices WHERE device = '$device_name' AND board = '$hardware_board' ORDER by version";
+	$query = mysql_query($sql);
+	
+	// build array with only version numbers
+	while($row = mysql_fetch_array($query)){
+		$versions[] = $row['version'];
+	}
+	
+	// sort them then grab the highest version number, beginning of the array
+	rsort($versions, SORT_NUMERIC);
+	$versions = array_reverse($versions);
+	$latest_version = $versions['0'];
+
+
+	if(version_compare($current_version, $latest_version) < 0){
+		//echo "success";
+		return true;
+	}else{
+		//echo "no dice";
+	}	
+	
 }
 
 // Function to get branch e.g. Stable or Dev
 function get_branch($hardware_board, $device_name, $version)
 {
-        // SQL
-	$sql = "SELECT branch FROM devices WHERE version='$version' AND device='$device_name' AND board='$hardware_board' LIMIT 1";
+	
+	// SQL
+	$sql = "SELECT branch FROM devices WHERE version='$version' AND device='$device_name' AND board='$hardware_board' LIMIT 
+1";
 
-        // Query
-        $query = mysql_query($sql)or die(mysql_error());
+	// Query
+	$query = mysql_query($sql)or die(mysql_error());
 
 	$branch = mysql_fetch_assoc($query);
 
@@ -36,25 +65,51 @@ function get_branch($hardware_board, $device_name, $version)
 // Function to return the latest version number
 function get_new_version($current_version)
 {
+	/*
 	// SQL
 	$sql = "SELECT version FROM devices WHERE version > '$current_version' LIMIT 1";
 
-        // Query
-        $query = mysql_query($sql)or die(mysql_error());
+	// Query
+	$query = mysql_query($sql)or die(mysql_error());
 
 	$version = mysql_fetch_assoc($query);
 
 	return $version['version'];
+	*/
+	
+	// select all versions for device
+	$sql = "SELECT version FROM devices ORDER by version";
+	$query = mysql_query($sql);
+	
+	// build array with only version numbers
+	while($row = mysql_fetch_array($query)){
+		$versions[] = $row['version'];
+	}
+	
+	// sort them then grab the highest version number, beginning of the array
+	rsort($versions, SORT_NUMERIC);
+	$versions = array_reverse($versions);
+	$latest_version = $versions['0'];
+
+
+	if(version_compare($current_version, $latest_version) < 0){
+		// current version is less than the latest version, return latest version
+		return $latest_version;
+	}else{
+		//
+	}		
+	
 }
 
 // Function to get the device name
 function get_device_name($hardware_board, $device_name, $new_version)
 {
-        // SQL
-	$sql = "SELECT * FROM devices WHERE version='$new_version' AND board='$hardware_board' AND device='$device_name' LIMIT 1";
+		// SQL
+	$sql = "SELECT * FROM devices WHERE version='$new_version' AND board='$hardware_board' AND device='$device_name' LIMIT 
+1";
 
-        // Query
-        $query = mysql_query($sql)or die(mysql_error());
+		// Query
+		$query = mysql_query($sql)or die(mysql_error());
 
 	$device_name = mysql_fetch_assoc($query);
 
@@ -66,9 +121,9 @@ function get_new_filesize($hardware_board, $device_name, $new_version)
 {
 	// SQL
 	$sql = "SELECT * FROM devices WHERE version='$new_version' AND board='$hardware_board' AND device='$device_name'";
-        
-        // Query
-        $query = mysql_query($sql)or die(mysql_error());
+		
+		// Query
+		$query = mysql_query($sql)or die(mysql_error());
 
 	$new_filesize = mysql_fetch_assoc($query);
 
@@ -77,25 +132,27 @@ function get_new_filesize($hardware_board, $device_name, $new_version)
 // Function to retrieve	current filesize details
 function get_filesize($hardware_board, $device_name, $current_version)
 {
-        // SQL
-	$sql = "SELECT * FROM devices WHERE version='$current_version' AND board='$hardware_board' AND device='$device_name' LIMIT 1";
+		// SQL
+	$sql = "SELECT * FROM devices WHERE version='$current_version' AND board='$hardware_board' AND device='$device_name' 
+LIMIT 1";
 
-        // Query
-        $query = mysql_query($sql)or die(mysql_error());
+		// Query
+		$query = mysql_query($sql)or die(mysql_error());
 
-        $filesize = mysql_fetch_assoc($query);
+		$filesize = mysql_fetch_assoc($query);
  
-        return $filesize['filesize'];
+		return $filesize['filesize'];
 }
 
 // Function to get the new file name for the OTA
 function get_new_filename($hardware_board, $device_name, $new_version)
 {
-        // SQL
- 	$sql = "SELECT * FROM devices WHERE version='$new_version' AND board='$hardware_board' AND device='$device_name' LIMIT 1";
-        
+		// SQL
+	$sql = "SELECT * FROM devices WHERE version='$new_version' AND board='$hardware_board' AND device='$device_name' LIMIT 
+1";
+		
 	// Query
-        $query = mysql_query($sql)or die(mysql_error());
+		$query = mysql_query($sql)or die(mysql_error());
 
 	$new_filename = mysql_fetch_assoc($query);
 
@@ -105,39 +162,42 @@ function get_new_filename($hardware_board, $device_name, $new_version)
 // Function to get the file name of the current file
 function get_filename($hardware_board, $device_name, $current_version)
 {
-        // SQL
-	$sql = "SELECT * FROM devices WHERE version='$current_version' AND board='$hardware_board' AND device='$device_name' LIMIT 1";
+		// SQL
+	$sql = "SELECT * FROM devices WHERE version='$current_version' AND board='$hardware_board' AND device='$device_name' 
+LIMIT 1";
 
-        // Query
-        $query = mysql_query($sql)or die(mysql_error());
+		// Query
+		$query = mysql_query($sql)or die(mysql_error());
 
-     	$filename = mysql_fetch_assoc($query);
+		$filename = mysql_fetch_assoc($query);
 
-        return $filename['filename'];
+		return $filename['filename'];
 }
 
 // Function to return the current files MD5 checksum
 function get_checksum($hardware_board, $device_name, $current_version)
 {
-        // SQL
-	$sql = "SELECT * FROM devices WHERE version='$current_version' AND board='$hardware_board' AND device='$device_name' LIMIT 1";
+		// SQL
+	$sql = "SELECT * FROM devices WHERE version='$current_version' AND board='$hardware_board' AND device='$device_name' 
+LIMIT 1";
 
-        // Query
-        $query = mysql_query($sql)or die(mysql_error());
+		// Query
+		$query = mysql_query($sql)or die(mysql_error());
 
-        $checksum = mysql_fetch_assoc($query);
+		$checksum = mysql_fetch_assoc($query);
 
-        return $checksum['checksum'];
+		return $checksum['checksum'];
 }
 
 // Function to return the new OTA files MD5 checksum
 function get_new_checksum($hardware_board, $device_name, $new_version)
 {
-        // SQL
-	$sql = "SELECT * FROM devices WHERE version='$new_version' AND board='$hardware_board' AND device='$device_name' LIMIT 1";
+		// SQL
+	$sql = "SELECT * FROM devices WHERE version='$new_version' AND board='$hardware_board' AND device='$device_name' LIMIT 
+1";
 
-        // Query
-        $query = mysql_query($sql)or die(mysql_error());
+		// Query
+		$query = mysql_query($sql)or die(mysql_error());
 
 	$new_checksum = mysql_fetch_assoc($query);
 
